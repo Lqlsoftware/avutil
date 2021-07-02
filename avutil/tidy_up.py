b3415b455e3c2e603310c150646743ae90049041
RobinLu <robin@lqlsoftware.cn>
2021-07-02 13:00:29 +0800

    Fix: Multi-threads support

--- avutil/tidy_up.py
+++ avutil/tidy_up.py
@@ -1,34 +1,27 @@
 #!/usr/bin/env python3
 import sys
 import argparse
+import threading
 import avutil
-
-from multiprocessing import Pool
 
 sys.setrecursionlimit(10000)
 
+src_folder = "./"
+dst_folder = "./"
+http_proxy = ""
+source = avutil.Library
+thread = 8
+with_poster = False
 
-def get_arguments(args=sys.argv[1:]):
-    parser = argparse.ArgumentParser(
-        description="Tidy up your personal video dir")
-    parser.add_argument("-r", dest='recursive', action='store_true',
-        help="search dir recursively")
-    parser.add_argument("-i", "--in", dest='IN',
-        help="video input dir")
-    parser.add_argument("-o", "--out", dest='OUT',
-        help="video output dir")
-    parser.add_argument("-p", "--proxy", dest='proxy',
-        help="http proxy address")
-    parser.add_argument("-s", "--source", dest='source',
-        help="data source of video info, 'library' or 'bus'")
-    parser.add_argument("-t", "--thread", dest='thread',
-        help="threads num, use multi-threads to download info & images")
-    parser.add_argument("--with-poster", dest='with_poster', action='store_true',
-        help="save poster")
-    return parser.parse_args(args)
 
-def VideoProcess(info):
-    designatio, file_paths = info
+def VideoProcess(designatio, file_paths):
+    global src_folder
+    global dst_folder
+    global http_proxy
+    global source
+    global thread
+    global with_poster
+
     try:
         video = avutil.Video(designatio, file_paths)
 
@@ -37,7 +30,8 @@ def VideoProcess(info):
         print(video.title)
 
         # Download cover
-        video.download_cover(dst_dir=dst_folder, http_proxy=http_proxy, with_poster=with_poster)
+        video.download_cover(dst_dir=dst_folder,
+                             http_proxy=http_proxy, with_poster=with_poster)
 
         # Tidy up
         video.rename(dst_dir=dst_folder)
@@ -48,32 +42,60 @@ def VideoProcess(info):
         print("WARN:", e)
         pass
 
+
+def get_arguments(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser(
+        description="Tidy up your personal video dir")
+    parser.add_argument("-r", dest='recursive', action='store_true',
+                        help="search dir recursively")
+    parser.add_argument("-i", "--in", dest='IN',
+                        help="video input dir")
+    parser.add_argument("-o", "--out", dest='OUT',
+                        help="video output dir")
+    parser.add_argument("-p", "--proxy", dest='proxy',
+                        help="http proxy address")
+    parser.add_argument("-s", "--source", dest='source',
+                        help="data source of video info, 'library' or 'bus'")
+    parser.add_argument("-t", "--thread", dest='thread', type=int,
+                        help="threads num, use multi-threads to download info & images")
+    parser.add_argument("--with-poster", dest='with_poster', action='store_true',
+                        help="save poster")
+    return parser.parse_args(args)
+
+
 def main():
-    
+
     global src_folder
     global dst_folder
     global http_proxy
     global source
+    global thread
     global with_poster
 
     args = get_arguments()
     # args.dir
-    if args.IN is None:
-        src_folder = './'
-    else:
+    if args.IN is not None:
         src_folder = args.IN
 
     # args.out
-    if args.OUT is None:
-        dst_folder = src_folder
-    else:
+    if args.OUT is not None:
         dst_folder = args.OUT
 
     # args.proxy
-    if args.proxy is None:
-        http_proxy = ""
-    else:
+    if args.proxy is not None:
         http_proxy = args.proxy
+
+    # Data source
+    if args.source == "bus":
+        source = avutil.Bus
+
+    # Gen poster
+    if args.with_poster == True:
+        with_poster = True
+
+    # Threads num
+    if args.thread is not None:
+        thread = int(args.thread)
 
     # Search folder
     if args.recursive == True:
@@ -81,23 +103,15 @@ def main():
     else:
         videos = avutil.Search_folder(src_folder)
 
-    # Data source
-    if args.source == "bus":
-        source = avutil.Bus
-    else:
-        source = avutil.Library
+    # Threads work
+    workers = []
+    for video in videos.items():
+        workers.append(threading.Thread(target=VideoProcess, args=video))
 
-    # Gen poster
-    if args.with_poster == True:
-        with_poster = True
-    else:
-        with_poster = False
+    # Start
+    for t in workers:
+        t.start()
 
-    # Threads num
-    if args.thread is None:
-        thread = 8
-    else:
-        thread = int(args.thread)
-
-    pool = Pool(thread)
-    pool.map(VideoProcess, videos.items())
+    # Join
+    for t in workers:
+        t.join()
